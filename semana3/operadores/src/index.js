4a923c0b8ad371846fbf1ff239d76366d81ae3d1
Daniel Melo <danielfarias380@gmail.com>
2020-07-28 11:51:27 -0300

    Resolução dos excercícios de interpretação

---
+++ semana3/operadores/src/index.js
@@ -0,0 +1,17 @@
+/* 
+EXERCÍCIOS DE INTERPRETAÇÃO
+
+1.
+a.  false
+b.  false
+c.  true
+e.  boolean
+
+2.
+a.  undefined
+b.  null
+c.  11
+d.  3
+e.  [3, 19, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+f.  9
+*/

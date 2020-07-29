ed3c2ac5a7d580645d49e2683950706af7102474
Daniel Melo <danielfarias380@gmail.com>
2020-07-29 11:52:52 -0300

    Respostas dos exercícios de interpretação de código

---
+++ semana3/condicionais/src/index.js
@@ -0,0 +1,26 @@
+/*
+EXERCÍCIOS DE INTERPRETAÇÃO DE CÓDIGO
+
+1. O código verifica se o número informado é um número par.
+Para isso, o programa verifica se o resto da divisão do número por 2 é igual a 0.
+Dessa forma, será impresso no console "Passou no teste" quando o número informado for um  número par,
+caso contrário, sera impresso "Não passou no teste"
+
+2.
+a. O código serve para informar ao usuário o preço da fruta informada como entrada.
+b. "O preço da fruta  Maçã  é  R$  2.25"
+c. "O preço da fruta  Pêra  é  R$  5"
+
+3. 
+a. A primeira linha pede uma entrada do usuário por meio da funçao "prompt". 
+Esta função retorna o valor dado como entrada em forma de string. A função Number
+é então utilizada para mudar o tipo desse valor para number.
+
+b. Se o usuário entrar com o valor 10, primeiro será impressa a mensagem 
+"Esse número passou no teste" e então será informado um erro avisando qua a variável
+mensagem não foi definida. 
+No caso do usuário entrar com o valor -10 só será apresentado o erro
+
+c. Sim, haverá erro. Uma vez que a variável 'mensagem' foi declarada dentro do bloco
+do if, ela não poderá ser acessada fora desse bloco, isto é, fora de seu escopo.
+*/

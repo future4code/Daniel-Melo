022c77090056c95b2b00150176fcc19ced774b4f
Daniel Melo <danielfarias380@gmail.com>
2020-07-31 12:05:37 -0300

    Finalização do jogo simples

--- semana3/blackjack/nossoJogo.js
+++ semana3/blackjack/nossoJogo.js
@@ -10,3 +10,57 @@
  * 
  * 
  */
+
+function jogar() {
+   const cartasUsuario = [comprarCarta(), comprarCarta()]
+   const cartasComputador = [comprarCarta(), comprarCarta()]
+   
+   const cartasUsuarioEmString = cartasToString(cartasUsuario)
+   const cartasComputadorEmString = cartasToString(cartasComputador)
+ 
+   const mensagemUsuario = `Usuário - ${cartasUsuarioEmString}`
+   const mensagemComputador = `Computador - ${cartasComputadorEmString}`
+
+   console.log(mensagemUsuario)
+   console.log(mensagemComputador)
+
+   determinarVencedor(cartasUsuario, cartasComputador)
+}
+
+function cartasToString(cartas) {
+   const pontos = contarPontos(cartas)
+   const texto = cartas.reduce((naipes, carta) => {
+      return `${naipes} ${carta.texto}`
+   },'')
+
+   return `cartas:${texto} - pontuação:${pontos}`
+}
+
+function determinarVencedor(cartasUsuario, cartasComputador) {
+   const pontosUsuario = contarPontos(cartasUsuario)
+   const pontosComputador = contarPontos(cartasComputador)
+
+   if (pontosUsuario > pontosComputador) {
+      console.log('O usuário venceu!')
+   } else if (pontosUsuario < pontosComputador) {
+      console.log('O computador venceu!') 
+   } else {
+      console.log('Empate!')
+   }
+}
+
+function contarPontos(cartas) {
+   return cartas.reduce((pontos, carta) => {
+      return pontos + carta.valor
+   }, 0)
+}
+
+
+
+console.log("Bem vindo ao jogo de Blackjack!")
+const iniciarJogo = confirm('Quer iniciar um jogo de blackjack?')
+
+iniciarJogo && jogar()
+
+console.log('O jogo acabou')
+

0cd246578ee8d2bfb739e3b1c119e63d165e17be
Daniel Melo <danielfarias380@gmail.com>
2020-07-31 13:54:37 -0300

    Resolução do desafio

--- semana3/blackjack/desafio.js
+++ semana3/blackjack/desafio.js
@@ -11,3 +11,107 @@
  * 
  */
 
+function jogar() {
+   const cartasUsuario = sortearCartas()
+   const cartasComputador = sortearCartas()
+   
+   rodadaDoUsuario(cartasUsuario, cartasComputador)
+   rodadaDoComputador(cartasUsuario, cartasComputador)
+
+   console.log(cartasUsuario)
+   console.log(cartasComputador)
+   const resultado = determinarVencedor(cartasUsuario, cartasComputador)
+
+   alert(resultado)
+}
+
+function sortearCartas() {
+   const cartas = []
+   
+   do {
+      cartas.push(comprarCarta())
+      cartas.push(comprarCarta())
+   } while(verificarDuploAs(cartas))
+   
+   return cartas
+}
+
+function verificarDuploAs(cartas) {
+   return 22 === contarPontos(cartas)
+}
+
+function rodadaDoUsuario(cartasUsuario, cartasComputador) {
+   while (contarPontos(cartasUsuario) < 21 && confirm(revelarCartas(cartasUsuario, cartasComputador))) {
+      cartasUsuario.push(comprarCarta())
+   }
+}
+
+function rodadaDoComputador(cartasUsuario, cartasComputador) {
+   const pontosUsuario = contarPontos(cartasUsuario)
+
+   while (pontosUsuario <= 21 && pontosUsuario > contarPontos(cartasComputador)) {
+      cartasComputador.push(comprarCarta())
+   }
+}
+
+function revelarCartas(cartasUsuario, cartasComputador) {
+   const cartasReveladasUsuario = revelarTodasAsCartas(cartasUsuario)
+   const cartaReveladaComputador = revelarPrimeiraCarta(cartasComputador)
+   mensagem = `Suas cartas são${cartasReveladasUsuario}.`
+   mensagem += ` As carta revelada do computador é ${cartaReveladaComputador}.`
+   mensagem += `\nDeseja comprar mais um carta?`
+   
+   return mensagem
+}
+
+function revelarTodasAsCartas(cartas) {
+   const texto = cartas.reduce((naipes, carta) => {
+      return `${naipes} ${carta.texto}`
+   },'')
+
+   return texto
+}
+
+function revelarPrimeiraCarta(cartas) {
+   return cartas[0].texto
+}
+
+function determinarVencedor(cartasUsuario, cartasComputador) {
+   const cartasReveladasUsuario = revelarTodasAsCartas(cartasUsuario)
+   const cartasReveladasComputador = revelarTodasAsCartas(cartasComputador)
+   
+   const pontosUsuario = contarPontos(cartasUsuario)
+   const pontosComputador = contarPontos(cartasComputador)
+
+   let mensagem = `Suas cartas são${cartasReveladasUsuario}.`
+   mensagem += ` Sua pontuação é ${pontosUsuario}.`
+   mensagem += ` As cartas do computador são${cartasReveladasComputador}.`
+   mensagem += ` A pontuação do computador é ${pontosComputador}.`
+
+   if (pontosUsuario > 21) {
+      mensagem += '\nO Computador ganhou!'
+   } else if (pontosComputador > 21) {
+      mensagem += '\nO Usuário ganhou!'
+   } else if (pontosUsuario > pontosComputador) {
+      mensagem += '\nO Usuário ganhou!'
+   } else if (pontosUsuario < pontosComputador) {
+      mensagem += '\nO Computador ganhou!'
+   } else {
+      mensagem += '\nEmpate!'
+   }
+
+   return mensagem
+}
+
+function contarPontos(cartas) {
+   return cartas.reduce((pontos, carta) => {
+      return pontos + carta.valor
+   }, 0)
+}
+
+console.log("Bem vindo ao jogo de Blackjack!")
+const iniciarJogo = confirm('Quer iniciar um jogo de blackjack?')
+
+iniciarJogo && jogar()
+
+console.log('O jogo acabou')

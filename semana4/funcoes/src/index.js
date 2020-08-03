2eb3313e328535aa1cffb1ace6c7d72b2203d3f4
Daniel Melo <danielfarias380@gmail.com>
2020-08-03 12:11:51 -0300

    Resposta do exercício 4

--- semana4/funcoes/src/index.js
+++ semana4/funcoes/src/index.js
@@ -13,3 +13,32 @@ b. Amanda e Caio, em linhas diferentes.
 Primeiro, essa função poderia ser dividida em duas, uma para filtrar os números pares do array, a qual poderíamos chama-lá de filtrarNumerosPares
 e outra função para elevar um número ao quadrado, poderíamos chamá-la elevarAoQuadrado.
 */
+
+// --- EXERCÍCIOS DE ESCRITA DE CÓDIGO ---
+
+// 4.
+// a.
+function apresentarCaio() {
+  console.log("Eu sou Caio, tenho 23 anos, moro em São Paulo e sou estudante")
+}
+
+function apresentarDaniel() {
+  console.log("Eu sou Daniel, tenho 25 anos, moro em Recife e sou estudante")
+}
+
+apresentarCaio()
+apresentarDaniel()
+
+// b.
+function apresentar(nome, idade, endereco, isEstudante) {
+  let souEstudante = ''
+  if (isEstudante) {
+    souEstudante = 'sou'
+  } else {
+    souEstudante = 'não sou'
+  }
+  
+  console.log(`Eu sou ${nome}, tenho ${idade} anos, moro em ${endereco} e ${souEstudante} estudante`)
+}
+
+apresentar('Daniel', 25, 'Av. Parnamirim 327', true)

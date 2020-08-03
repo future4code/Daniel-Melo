9862c85587618053c07c8ceadf3e8783ee24e358
Daniel Melo <danielfarias380@gmail.com>
2020-08-03 11:40:32 -0300

    Respostas dos exercícios de escrita de código

---
+++ semana4/funcoes/src/index.js
@@ -0,0 +1,15 @@
+/*
+--- EXERCÍCIOS DE INTERPRETAÇÃO DE CÓDIGO ---
+
+1.
+a. será impresso 10 e 50, em linhas diferentes.
+b. A função iria ser chamada mas não ia ser feito nada com o valor retornado, nada seria impresso no console.
+
+2.
+a. Darvas e Goli, em linhas diferentes.
+b. Amanda e Caio, em linhas diferentes.
+
+3. Retorna um novo array com o quadrado dos números pares do array passado como argumento.
+Primeiro, essa função poderia ser dividida em duas, uma para filtrar os números pares do array, a qual poderíamos chama-lá de filtrarNumerosPares
+e outra função para elevar um número ao quadrado, poderíamos chamá-la elevarAoQuadrado.
+*/

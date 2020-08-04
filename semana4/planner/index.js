6f159041e01df0462de3d1e569492b2423d6de8e
Daniel Melo <danielfarias380@gmail.com>
2020-08-04 13:53:56 -0300

    Impede a criação de tarefa em branco

--- semana4/planner/index.js
+++ semana4/planner/index.js
@@ -5,9 +5,13 @@ function criarTarefa() {
   const task = taskInput.value
   const weekDay = weekDaySelector.value
 
-  const taskListContainer = document.getElementById(`${weekDay}`)
-
-  taskListContainer.innerHTML += `<li>${task}</li>`
-
-  taskInput.value = ""
+  if (task !== '') {
+    const taskListContainer = document.getElementById(`${weekDay}`)
+  
+    taskListContainer.innerHTML += `<li>${task}</li>`
+  
+    taskInput.value = ""
+  } else {
+    alert('Tarefa inválida')
+  }
 }

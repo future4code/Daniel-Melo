ac71ae28d04cf9c5f2d529fee9ccccf02e6731df
Daniel Melo <danielfarias380@gmail.com>
2020-08-05 14:21:53 -0300

    Fix: Cria uma nova tag article para cada post

--- semana4/objetos/blog/js/posts.js
+++ semana4/objetos/blog/js/posts.js
@@ -1,10 +1,10 @@
 const posts = JSON.parse(sessionStorage.posts)
 
 const postsContainer = document.getElementById('container-de-posts')
-const postCard = document.createElement('article')
-postCard.className = 'post'
 
 for (let i = posts.length - 1; i >= 0; i--) {
+  const postCard = document.createElement('article')
+  postCard.className = 'post'
   postCard.innerHTML += `<img src="${posts[i].image}" alt="post image">`
   postCard.innerHTML += `<h2>${posts[i].title}</h2>`
   postCard.innerHTML += `<h3>${posts[i].author}</h3>`

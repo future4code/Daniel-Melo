c42b1d71cc6b02bf34aab84f67ef3cabc3417036
Daniel Melo <danielfarias380@gmail.com>
2020-08-05 15:00:27 -0300

    Fix: Apresentação dos posts dos mais novos para os mais antigos

--- semana4/objetos/blog/js/posts.js
+++ semana4/objetos/blog/js/posts.js
@@ -2,13 +2,13 @@ const posts = JSON.parse(sessionStorage.posts)
 
 const postsContainer = document.getElementById('container-de-posts')
 
-for (let i = posts.length - 1; i >= 0; i--) {
+for (post of posts) {
   const postCard = document.createElement('article')
   postCard.className = 'post'
-  postCard.innerHTML += `<img src="${posts[i].image}" alt="post image">`
-  postCard.innerHTML += `<h2>${posts[i].title}</h2>`
-  postCard.innerHTML += `<h3>${posts[i].author}</h3>`
-  postCard.innerHTML += `<p>${posts[i].content}</p>`
+  postCard.innerHTML += `<img src="${post.image}" alt="post image">`
+  postCard.innerHTML += `<h2>${post.title}</h2>`
+  postCard.innerHTML += `<h3>${post.author}</h3>`
+  postCard.innerHTML += `<p>${post.content}</p>`
   
   postsContainer.insertAdjacentElement("afterbegin", postCard)
 }

23f0152b79f929676e24a1ceb515ffe4ccb5dd1a
Daniel Melo <danielfarias380@gmail.com>
2020-08-07 11:26:21 -0300

    Reposta dos exercícios de interpretação de código

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,23 @@
+/*
+--- Exercícios de interpretação de código ---
+1. A função converte o valor em dolar para Real, cuja cotação é passada pelo usuário, e retorna um string no formato R$ <valor em real>.
+Por exmplo, se a cotação for de R$ 5,00, o mensagem do console será R$ 500.
+
+2. A função recebe dois parâmetros, o primeito é uma string com o tipo de investimento e o segundo o valor investido.
+Se o tipo de investimento informado for válido, a função retorna o montante total após os rendimentos.
+No exemplo do código, será impresso no console:
+165
+undefined (Nesse caso, também será dado um alerta ao usuário: TIPO DE INVESTIMENTO INFORMADO INCORRETO!)
+
+3. O código percorre todos os elementos do array numeros, se o elemento for um numero par, o valor é inserido também no array1,
+se for ímpar, o valor é inserido também no array2.
+No console será impresso:
+Quatidade total de números: 14
+6
+8
+
+4. O código verifica o elemento de maior (numero2) e menor (numero1) valor.
+No console será impresso:
+-10
+1590
+*/

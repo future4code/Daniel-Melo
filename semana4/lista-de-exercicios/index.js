800d31450bc876788b7c5984e3e5289d36235de7
Daniel Melo <danielfarias380@gmail.com>
2020-08-07 12:56:07 -0300

    Respostas dos exercícios de lógica de programação

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -21,3 +21,73 @@ No console será impresso:
 -10
 1590
 */
+
+// --- EXERCÍCIOS DE LÓGICA DE PROGRAMAÇÃO ---
+
+// 1. Utilizado for, for...of ou forEach.
+
+// maneirasDeIterarSobreUmaLista()
+
+function maneirasDeIterarSobreUmaLista() {
+  const lista = ['a', 'b', 'c', 'd', 'e']
+
+  console.log('Utilizando for')
+  for (let index = 0; index < lista.length; index++){
+    console.log(lista[index])
+  }
+
+  console.log('Utilizando for...of')
+  for (let elemento of lista) {
+    console.log(elemento)
+  }
+
+  console.log('Utiliando forEach')
+  lista.forEach((elemento, index, array) => {
+    console.log(elemento)
+  })
+}
+
+// 2. a) false b) false c) true d) true e) true 
+
+// 3. Não funciona, na verdade ele gerou um loop infinito uma vez que não incremeta o valor do contador i.
+// A condição também precisa ser < e não <=, uma vez que o valor 0 também precisa ser considerado.
+// Além disso, em momento algum ele pediu a entrada do usuário.
+// Apesar de ser possível resolver o problema com a estrutura while, nesse caso é mais adequado usar a estrutura for,
+// dado que o número de repetições é previamente conhecido.
+
+// imprimirNPrimeirosValoresPares()
+
+function imprimirNPrimeirosValoresPares() {
+  const quantidadeDeNumerosPares = Number(prompt('Digite a quantidade de números pares'))
+  for (let e = 0; e < quantidadeDeNumerosPares; e++) {
+    console.log(e*2)
+  }
+} 
+
+// 4. 
+// console.log(classficiarTriangulo(1, 1, 1))
+// console.log(classficiarTriangulo(1, 2, 3))
+// console.log(classficiarTriangulo(1, 2, 2))
+
+function classficiarTriangulo(a, b, c) {
+  if ( a === b && a === c) {
+    return 'Equilátero'
+  } else if ( a !== b && a !== c && c !== b) {
+    return 'Escaleno'
+  } else {
+    return 'Isóceles'
+  }
+}
+
+// 5.
+// exercicioCinco(30, 15)
+
+function exercicioCinco(a, b) {
+  console.log('ENTRADA:')
+  console.log(`${a} e ${b}`)
+  console.log('\nSAÍDA:')
+  console.log(`O maior é: ${a > b ? a : b}`)
+  console.log(`${a} ${a % b === 0 ? 'é' : 'não é'} divisível por ${b}`)
+  console.log(`${b} ${b % a === 0 ? 'é' : 'não é'} divisível por ${a}`)
+  console.log(`A diferença entre ele é ${Math.abs(a - b)}`)
+} 

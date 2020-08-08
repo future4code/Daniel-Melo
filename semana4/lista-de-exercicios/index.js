a9c985046622fd631319977a585e21509223485f
Daniel Melo <danielfarias380@gmail.com>
2020-08-08 08:49:35 -0300

    Respostas dos exercicios de funções de array

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -176,3 +176,120 @@ const pessoa = {
 function anonimizarPessoa(pessoa) {
   return { ...pessoa, nome: 'ANÔNIMO'}
 }
+
+// --- EXERCÍCIOS DE FUNÇÕES DE ARRAY ---
+// 1.
+// exercicio1()
+
+function exercicio1() {
+  const pessoas = [
+    { nome: "Pedro", idade: 20 },
+    { nome: "João", idade: 10 },
+    { nome: "Paula", idade: 12 },
+    { nome: "Artur", idade: 89 } 
+  ]
+  
+  console.log(soAdultos(pessoas))
+  console.log(soCriancas(pessoas))
+  
+  function soAdultos(array) {
+    return array.filter(pessoa => pessoa.idade >= 20)
+  }
+  
+  function soCriancas(array) {
+    return array.filter(pessoa => pessoa.idade < 20)
+  }
+}
+
+// 2.
+// exercicio2()
+
+function exercicio2() {
+  const array = [1, 2, 3, 4, 5, 6]
+  
+  console.log(vezes2(array))
+  console.log(vezes3ToString(array))
+  console.log(imparOuPar(array))
+  
+  function vezes2(array) {
+    return array.map(e => e * 2)
+  }
+  
+  function vezes3ToString(array) {
+    return array.map(e => `${e * 3}`)
+  }
+  
+  function imparOuPar(array) {
+    return array.map(e => `${e} é ${e % 2 === 0 ? 'par' : 'ímpar'}`)
+  }
+}
+
+// 3.
+// exercicio3()
+
+function exercicio3() {
+  const pessoas = [
+    { nome: "Paula", idade: 12, altura: 1.8},
+    { nome: "João", idade: 20, altura: 1.3},
+    { nome: "Pedro", idade: 15, altura: 1.9},
+    { nome: "Luciano", idade: 22, altura: 1.8},
+    { nome: "Artur", idade: 10, altura: 1.2},
+    { nome: "Soter", idade: 70, altura: 1.9}
+  ]
+
+  console.log(permitirEntrada(pessoas))
+  console.log(proibirEntrada(pessoas))
+
+  function permitirEntrada(array) {
+    return array.filter(pessoa => pessoa.idade > 14 && pessoa.idade < 60 && pessoa.altura >= 1.5)
+  }
+  
+  function proibirEntrada(array) {
+    return array.filter(pessoa => !(pessoa.idade > 14 && pessoa.idade < 60 && pessoa.altura >= 1.5))
+  }
+}
+
+// 4.
+// exercicio4()
+
+function exercicio4() {
+  const consultas = [
+    { nome: "João", genero: "masculino", cancelada: true, dataDaConsulta: "01/10/2019" },
+    { nome: "Pedro", genero: "masculino", cancelada: false, dataDaConsulta: "02/10/2019" },
+    { nome: "Paula", genero: "feminino", cancelada: true, dataDaConsulta: "03/11/2019" },
+    { nome: "Márcia", genero: "feminino", cancelada: false, dataDaConsulta: "04/11/2019" }
+  ]
+
+  const emails = consultas.map(consulta => {
+    const pronome = consulta.genero === 'masculino' ? 'Sr.' : 'Sra.'
+    const lembrete = consulta.genero === 'masculino' ? 'lembrá-lo' : 'lembrá-la'
+
+    const mensagemConfirmacao = `Olá, ${pronome} ${consulta.nome}. Estamos enviando esta mensagem para ${lembrete} da sua consulta no dia ${consulta.dataDaConsulta}. Por favor, acuse o recebimento desse e-mail`
+    const mensagemCancelamento = `Olá, ${ pronome } ${ consulta.nome }. Infelizmente, sua consulta marcada para o dia ${ consulta.dataDaConsulta } foi cancelada. Se quiser, pode entrar em contato conosco para remarcá-la`
+  
+    return consulta.cancelada ? mensagemCancelamento : mensagemConfirmacao
+  })
+
+  console.log(emails)
+}
+
+// 5.
+// exercicio5()
+
+function exercicio5() {
+  const contas = [
+    { cliente: "João", saldoTotal: 1000, compras: [100, 200, 300] },
+    { cliente: "Paula", saldoTotal: 7500, compras: [200, 1040] },
+    { cliente: "Pedro", saldoTotal: 10000, compras: [5140, 6100, 100, 2000] },
+    { cliente: "Luciano", saldoTotal: 100, compras: [100, 200, 1700] },
+    { cliente: "Artur", saldoTotal: 1800, compras: [200, 300] },
+    { cliente: "Soter", saldoTotal: 1200, compras: [] }
+  ]
+
+  atualizarSaldoTotal(contas)
+  console.table(contas)
+
+  function atualizarSaldoTotal(contas) {
+    contas.forEach(conta => conta.saldoTotal -= conta.compras.reduce((total, compra) => total + compra, 0))
+  }
+}

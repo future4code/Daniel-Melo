bfac58dd838848114eaf426bac5be482868fa009
Daniel Melo <danielfarias380@gmail.com>
2020-08-12 12:57:25 -0300

    Cria mais 2 posts

--- semana5/insta4/src/App.js
+++ semana5/insta4/src/App.js
@@ -11,6 +11,16 @@ class App extends React.Component {
           fotoUsuario={'https://picsum.photos/50/50'}
           fotoPost={'https://picsum.photos/200/150'}
         />
+        <Post
+          nomeUsuario={'daniel'}
+          fotoUsuario={'https://picsum.photos/51/51'}
+          fotoPost={'https://picsum.photos/201/151'}
+        />
+        <Post
+          nomeUsuario={'simone'}
+          fotoUsuario={'https://picsum.photos/52/52'}
+          fotoPost={'https://picsum.photos/202/152'}
+        />
       </div>
     );
   }

8a21193504ae46ea2139d006a72fdb4f093de72b
Daniel Melo <danielfarias380@gmail.com>
2020-08-17 15:38:27 -0300

    Criação da página da Etapa 4

---
+++ semana6/labenu-forms/src/pages/Etapa4.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+import StyledPage from '../components/StyledPage'
+
+class Etapa4 extends Component {
+  render() {
+    return (
+      <StyledPage>
+        <h1>O FORMULÁRIO ACABOU</h1>
+        <p>Muito obrigado por participar! Entraremos em contato!</p>
+      </StyledPage>
+    )
+  }
+}
+
+export default Etapa4;

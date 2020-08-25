3fe20b9716c9880707984ad4b63aaa5b02921298
Daniel Melo <danielfarias380@gmail.com>
2020-08-25 11:50:07 -0300

    Configurando estilos globais

--- semana7/labenusers/src/App.js
+++ semana7/labenusers/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import GlobalStyles from './styles/GlobalStyles'
+
 function App() {
   return (
-    <div className="App">
+    <>
+      <GlobalStyles />
       Hello, World!
-    </div>
+    </>
   );
 }
 
---
+++ semana7/labenusers/src/styles/GlobalStyles.js
@@ -0,0 +1,30 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+  
+  body {
+    margin: 0;
+    padding: 0;
+    height: 100vh;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+  
+  #root {
+    margin: 0;
+    padding: 0;
+    height: 100%;
+    width: 100%;
+  }
+
+  code {
+    font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+    monospace;
+  }
+`

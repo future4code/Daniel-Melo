a27034030cc64fccac5a9ad43864949c69912d70
Daniel Melo <danielfarias380@gmail.com>
2020-08-26 15:37:23 -0300

    Refatoração e Estilização do componente Header

--- semana7/labenusers/src/components/Header/styles.js
+++ semana7/labenusers/src/components/Header/styles.js
@@ -7,3 +7,17 @@ export const Container = styled.div`
   align-items: center;
   justify-content: space-between;
 `
+
+export const StyledButton = styled.button`
+  font-size: 0.95rem;
+  border: 1px solid var(--secondary);
+  border-radius: 20px;
+  padding: 1em;
+  color: var(--secondary);
+  background: transparent;
+  transition: background-color 500ms ease-in-out;
+
+  :hover {
+    background-color: var(--independece);
+  }
+`

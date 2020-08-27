a2926804021045561b6d17c084936c56cd9e65b1
Daniel Melo <danielfarias380@gmail.com>
2020-08-27 12:32:05 -0300

    create Main component

---
+++ semana7/using-apis/src/components/Main/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { Container } from './styles'
+
+const Main = () => {
+  return (
+    <Container>
+      Main
+    </Container>
+  )  
+}
+
+export default Main
---
+++ semana7/using-apis/src/components/Main/styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const Container = styled.main`
+  flex-grow: 1;
+  
+  border: 1px solid red;
+`

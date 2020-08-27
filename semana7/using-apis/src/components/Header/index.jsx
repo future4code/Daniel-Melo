fa052343246a429494c4eaa23a26ec1684014fdb
Daniel Melo <danielfarias380@gmail.com>
2020-08-27 12:31:52 -0300

    create Header component

---
+++ semana7/using-apis/src/components/Header/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { Container } from './styles'
+
+const Header = () => {
+  return (
+    <Container>
+      Header
+    </Container>
+  )  
+}
+
+export default Header

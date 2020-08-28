74b2124eb704538ec25b1cc023bc72cd6e6adcc4
Daniel Melo <danielfarias380@gmail.com>
2020-08-28 15:03:31 -0300

    criação do componente TrackItem

---
+++ semana7/labefy/src/components/PlaylistDetail/TrackItem/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+import { Container } from './styles'
+
+const TrackItem = (props) => {
+  return (
+    <Container>
+      <td>{props.title}</td>
+      
+      <td>{props.artists}</td>
+      
+      <td>
+        <audio controls>
+          <source src={props.source} type="audio/mpeg" />
+          Your browser does not support the audio tag.
+        </audio>
+      </td>
+      
+      <td>
+        <button>Excluir</button>
+      </td>
+    </Container>
+  )
+}
+
+export default TrackItem
---
+++ semana7/labefy/src/components/PlaylistDetail/TrackItem/styles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+export const Container = styled.tr`
+  :hover {
+    background-color: #f5f5f5;
+  }
+
+  td {
+    text-align: center;
+    border-bottom: 1px solid #ddd;
+    padding: 5px 0;
+  }
+
+
+  audio {
+    outline: none;
+  }
+`

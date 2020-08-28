9bfd0888e42d1f3c0aa6481c1af7246f20587f66
Daniel Melo <danielfarias380@gmail.com>
2020-08-28 15:53:20 -0300

    importação do TracksTable para PlaylistDetail

--- semana7/labefy/src/components/PlaylistDetail/index.jsx
+++ semana7/labefy/src/components/PlaylistDetail/index.jsx
@@ -1,64 +1,128 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { 
+  getPlaylistTracks,
+  removeTrackFromPlaylist,
+  addTrackToPlaylist
+} from '../../services/api'
+import { axiosErrorHandler } from '../../utils/axiosErrorHandler'
 
 import { Container } from './styles'
 
-const PlaylistDetail = () => {
-  return (
-    <Container>
-      <h1>Playlist</h1>
-      <button>Voltar</button>
-      <table>
-        <thead>
-          <tr>
-            <th>Título</th>
-            <th>Artistas</th>
-            <th>Player</th>
-            <th>Actions</th>
-          </tr>
+import TracksTable from './TracksTable'
 
-          <tr>
-            <td>Título</td>
-            <td>Artistas</td>
-            <td>
-              <audio controls>
-                <source src="http://spoti4.future4.com.br/1.mp3" type="audio/mpeg" />
-                Your browser does not support the audio tag.
-              </audio>
-            </td>
-            <td>
-              <button>Excluir</button>
-            </td>
-          </tr>
-          <tr>
-            <td>Título</td>
-            <td>Artistas</td>
-            <td>
-              <audio controls>
-                <source src="http://spoti4.future4.com.br/1.mp3" type="audio/mpeg" />
-                Your browser does not support the audio tag.
-              </audio>
-            </td>
-            <td>
-              <button>Excluir</button>
-            </td>
-          </tr>
-          <tr>
-            <td>Título</td>
-            <td>Artistas</td>
-            <td>
-              <audio controls>
-                <source src="http://spoti4.future4.com.br/1.mp3" type="audio/mpeg" />
-                Your browser does not support the audio tag.
-              </audio>
-            </td>
-            <td>
-              <button>Excluir</button>
-            </td>
-          </tr>
-        </thead>
-      </table>
-    </Container>
-  )
+const initialState = {
+  titleInput: '',
+  artistInput: '',
+  urlInput: '',
+  tracks: []
+}
+
+class PlaylistDetail extends Component {
+  constructor(props) {
+    super(props)
+    this.state = initialState
+  }
+
+  componentDidMount = () => {
+    this.setPlaylistTracks()
+  }
+
+  setPlaylistTracks = async () => {
+    try {
+      const response = await getPlaylistTracks('bd0019d1-bdac-40ed-a804-7392329372e6')
+      const tracks = response.data.result.tracks
+
+      this.setState({tracks})
+    } catch (error) {
+      axiosErrorHandler(error)
+    }
+  }
+
+  addTrack = async (e) => {
+    e.preventDefault()
+
+    try {
+      await addTrackToPlaylist(
+        'bd0019d1-bdac-40ed-a804-7392329372e6',
+        this.state.titleInput,
+        this.state.artistInput,
+        this.state.urlInput
+      )
+      this.setPlaylistTracks()
+    } catch (error) {
+      axiosErrorHandler(error)
+    }
+
+    this.clear()
+  }
+
+  deleteTrack = async (trackId) => {
+    try{
+      await removeTrackFromPlaylist('bd0019d1-bdac-40ed-a804-7392329372e6', trackId)
+      this.setPlaylistTracks()
+    } catch (error) {
+      axiosErrorHandler(error)
+    }
+  }
+
+  handleTitleInputChange = (e) => {
+    this.setState({ titleInput: e.target.value })
+  }
+
+  handleArtistInputChange = (e) => {
+    this.setState({ artistInput: e.target.value })
+  }
+
+  handleUrlInputChange = (e) => {
+    this.setState({ urlInput: e.target.value })
+  }
+
+  clear = () => {
+    this.setState({
+      titleInput: '',
+      artistInput: '',
+      urlInput: ''
+    })
+  }
+
+  render() {
+    return (
+      <Container>
+        <h1>Playlist</h1>
+  
+        <form onSubmit={this.addTrack} onReset={this.clear}>
+          <input
+            type="text"
+            value={this.state.titleInput}
+            onChange={this.handleTitleInputChange}
+            placeholder="Título"
+            required
+          />
+          <input
+            type="text"
+            value={this.state.artistInput}
+            onChange={this.handleArtistInputChange}
+            placeholder="Artistas"
+            required
+          />
+          <input
+            type="text"
+            value={this.state.urlInput}
+            onChange={this.handleUrlInputChange}
+            placeholder="URL"
+            required
+          />
+          <button type="submit">Adicionar música</button>
+          <button type="reset">Limpar</button>
+        </form>
+  
+        <TracksTable
+          tracks={this.state.tracks}
+          onDelete={this.deleteTrack}          
+        />
+      </Container>
+    )
+  }
 }
 
 export default PlaylistDetail
--- semana7/labefy/src/components/PlaylistDetail/styles.js
+++ semana7/labefy/src/components/PlaylistDetail/styles.js
@@ -1,40 +1,25 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-  border: 1px solid black;
   display: grid;
   grid-template-columns: repeat(3, 1fr);
-  grid-template-rows: 50px 1fr;
+  grid-template-rows: 50px 50px 1fr;
   gap: 10px;
   grid-template-areas: 
-    'title . backButton'
+    '. title .'
+    'addTrack addTrack addTrack'
     'table table table'  
   ;
   padding: 10px 12.5vw;
 
   h1 {
+    text-align: center;
     grid-area: title;
     margin-bottom: 20px;
   }
 
-  button {
-    grid-area: backButton;
-  }
-
-  table {
-    grid-area: table;
-    border-collapse: collapse;
-  }
-
-  td {
+  form {
     text-align: center;
-    border-bottom: 1px solid #ddd;
-    padding: 5px 0;
-  }
-
-  tr:hover {background-color: #f5f5f5;}
-
-  audio {
-    outline: none;
+    grid-area: addTrack;
   }
 `

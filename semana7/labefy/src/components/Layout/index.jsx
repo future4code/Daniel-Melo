ec4871c919c07081025c3305417165cf8b0cca4b
Daniel Melo <danielfarias380@gmail.com>
2020-08-28 11:33:42 -0300

    initial files and structure

---
+++ semana7/labefy/src/components/Layout/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { Container } from './styles'
+
+const Layout = () => {
+  return (
+    <Container>
+      Layout
+    </Container>
+  )
+}
+
+export default Layout

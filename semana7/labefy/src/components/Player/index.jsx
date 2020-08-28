5df89e236c86fa8d6b44cedf0d1d0d9e2525ab44
Daniel Melo <danielfarias380@gmail.com>
2020-08-28 11:44:39 -0300

    criação do componente Player

---
+++ semana7/labefy/src/components/Player/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { Container } from './styles'
+
+const Footer = () => {
+  return (
+    <Container>
+      Footer
+    </Container>
+  )
+}
+
+export default Footer

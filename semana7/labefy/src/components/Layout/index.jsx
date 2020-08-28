59a5046e9d29dd232a1b28f0b216d00e973e95cd
Daniel Melo <danielfarias380@gmail.com>
2020-08-28 11:44:52 -0300

    importação dos componentes no Layout

--- semana7/labefy/src/components/Layout/index.jsx
+++ semana7/labefy/src/components/Layout/index.jsx
@@ -2,10 +2,16 @@ import React from 'react'
 
 import { Container } from './styles'
 
+import Header from '../Header'
+import Main from '../Main'
+import Player from '../Player'
+
 const Layout = () => {
   return (
     <Container>
-      Layout
+      <Header />
+      <Main />
+      <Player />
     </Container>
   )
 }

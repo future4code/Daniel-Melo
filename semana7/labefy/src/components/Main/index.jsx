0738130246c0ce9ba12fb6059096a0fc16fc0e0a
Daniel Melo <danielfarias380@gmail.com>
2020-08-28 14:01:31 -0300

    configuração da renderização das páginas

--- semana7/labefy/src/components/Main/index.jsx
+++ semana7/labefy/src/components/Main/index.jsx
@@ -1,15 +1,38 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import { Container } from './styles'
 
 import Playlists from '../Playlists'
 
-const Main = () => {
-  return (
-    <Container>
-      <Playlists />
-    </Container>
-  )
+const initialState = {
+  page: 'PLAYLISTS'
+}
+
+class Main extends Component {
+  constructor(props) {
+    super(props)
+    this.state = initialState
+  }
+  
+  setPageToRender = () => {
+    switch (this.state.page) {
+      case 'PLAYLISTS':
+        return <Playlists />
+      case 'DETAIL':
+        return <h1>Under construction</h1>
+      default:
+        return <Playlists />
+    }
+  }
+
+  render() {
+    return (
+      <Container>
+        {this.setPageToRender()}
+      </Container>
+    )
+  }
+  
 }
 
 export default Main

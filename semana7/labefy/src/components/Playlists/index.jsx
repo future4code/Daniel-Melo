daa0d390d2334818d83569c2abff6ae228263b33
Daniel Melo <danielfarias380@gmail.com>
2020-08-28 12:03:28 -0300

    criação do componente Playlists

---
+++ semana7/labefy/src/components/Playlists/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { Container } from './styles'
+
+const Playlists = () => {
+  return (
+    <Container>
+      Playlists
+    </Container>
+  )
+}
+
+export default Playlists

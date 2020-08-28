6afd6a37826507abaa2e58cb1d42d04092907b6c
Daniel Melo <danielfarias380@gmail.com>
2020-08-28 15:52:53 -0300

    exporta funcoes de buscar, adicionar e remover musica da playlist

--- semana7/labefy/src/services/api.js
+++ semana7/labefy/src/services/api.js
@@ -13,6 +13,14 @@ export const createPlaylist = (name) => api.post('/playlists', { name })
 
 export const deletePlaylist = (id) => api.delete(`/playlists/${id}`)
 
-export const getPlaylistTracks = (id) => api.get(`playlists/${id}/tracks`)
+export const getPlaylistTracks = (id) => api.get(`/playlists/${id}/tracks`)
+
+export const addTrackToPlaylist = (playlistId, name, artist, url) => (
+  api.post(`/playlists/${playlistId}/tracks`, { name, artist, url })
+)
+
+export const removeTrackFromPlaylist = (playlistId, trackId) => (
+  api.delete(`/playlists/${playlistId}/tracks/${trackId}`)
+) 
 
 export default api

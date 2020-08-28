1a5003d96a149d5d4c495ce830678c7ea44eeecc
Daniel Melo <danielfarias380@gmail.com>
2020-08-28 11:50:07 -0300

    organização dos componente do layout

--- semana7/labefy/src/components/Layout/styles.js
+++ semana7/labefy/src/components/Layout/styles.js
@@ -1,5 +1,10 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
+  min-height: 100vh;
+  
+  display: flex;
+  flex-direction: column;
+
   border: 1px solid black;
 `

b2101f05d587d570b51f82fbdb49645fd1e003c5
Daniel Melo <danielfarias380@gmail.com>
2020-08-28 12:33:12 -0300

    criação do componente Playlists com dados mockados

--- semana7/labefy/src/components/Playlists/index.jsx
+++ semana7/labefy/src/components/Playlists/index.jsx
@@ -1,13 +1,66 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import { Container } from './styles'
 
-const Playlists = () => {
-  return (
-    <Container>
-      Playlists
-    </Container>
-  )
+import PlayListCard from './PlaylistCard'
+
+class Playlists extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      playlists: [
+        {
+          id: 1,
+          name: "Playlist 1"
+        },
+        {
+          id: 2,
+          name: "Playlist 1"
+        },
+        {
+          id: 3,
+          name: "Playlist 1"
+        },
+        {
+          id: 4,
+          name: "Playlist 1"
+        },
+        {
+          id: 5,
+          name: "Playlist 1"
+        },
+        {
+          id: 6,
+          name: "Playlist 1"
+        },
+        {
+          id: 7,
+          name: "Playlist 1"
+        },
+        {
+          id: 8,
+          name: "Playlist 1"
+        },
+      ]
+    }
+  }
+  
+  render () {
+    return (
+      <Container>
+        {
+          this.state.playlists.map((playlist, i) => (
+            <PlayListCard 
+              key={playlist.id}
+              image={`https://picsum.photos/200?a=${i}`}
+              name={playlist.name}
+            />
+          ))
+        }
+      </Container>
+    )
+  }
 }
 
 export default Playlists
--- semana7/labefy/src/components/Playlists/styles.js
+++ semana7/labefy/src/components/Playlists/styles.js
@@ -2,4 +2,9 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
   border: 1px solid black;
+  padding: 10px 12.5vw;
+
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 10px;
 `

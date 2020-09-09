08eedb6b0e3ca1617fe1c0fc967dd128ef849881
Daniel Melo <danielfarias380@gmail.com>
2020-09-09 18:04:44 -0300

    create Content component

---
+++ semana9/astromatch/src/components/Content.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import Box from '@material-ui/core/Box'
+import ChoosePage from './ChoosePage'
+
+const Content = () => {
+  return (
+    <Box
+      m={2}
+    >
+      <ChoosePage />
+    </Box>
+  )
+}
+
+export default Content

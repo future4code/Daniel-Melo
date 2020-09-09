da73a47392cdb40a06f5232a248562161798e538
Daniel Melo <danielfarias380@gmail.com>
2020-09-09 16:04:16 -0300

    create ThemeContext

---
+++ semana9/astromatch/src/context/ThemeContext.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles'
+
+const ThemeContext = ({ children }) => {
+  const theme = createMuiTheme({
+        palette: {
+          primary: {
+            main: '#50B2C0'
+          },
+          secondary: {
+            main: '#FF4000'
+          },
+          text: {
+            primary: '#201E1F'
+          },
+          background: { 
+            default: '#FEEFDD'
+          }
+        },
+        spacing: [0, 15, 30, 60, 90, 120]
+      })
+    
+  return (
+    <ThemeProvider theme={theme}>
+      {children}
+    </ThemeProvider>
+  )
+}
+
+export default ThemeContext

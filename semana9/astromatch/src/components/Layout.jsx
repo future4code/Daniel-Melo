2bbb5b6fcdca1ee7a3606a746f38d20d1ce66514
Daniel Melo <danielfarias380@gmail.com>
2020-09-09 17:15:57 -0300

    create Layout component and import Header

---
+++ semana9/astromatch/src/components/Layout.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Box } from '@material-ui/core'
+
+import Header from './Header'
+import { common } from '@material-ui/core/colors'
+
+const Layout = () => {
+  return (
+    <Box
+     width={430}
+     height={680}
+     border={1}
+     borderRadius={8}
+     bgcolor={common.white}
+    >
+      <Header></Header>
+    </Box>
+  )
+}
+
+export default Layout

965a35508c25f86faef2767353c1925c42f45d65
Daniel Melo <danielfarias380@gmail.com>
2020-09-09 17:24:17 -0300

    set icon visibility

--- semana9/astromatch/src/components/Header.jsx
+++ semana9/astromatch/src/components/Header.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 import Box from '@material-ui/core/Box'
+import styled from 'styled-components'
+
 import chooseUserIcon from '../assets/img/mdi_account-switch.svg'
 import logo from '../assets/img/logo.svg'
 import userMatchesIcon from '../assets/img/mdi_account-check.svg'
+
+const IconImg = styled.img`
+  cursor: pointer;
+  visibility: ${(props) => props.active ? 'visible' : 'hidden' };
+`
 
 const Header = () => {
   return (
@@ -15,9 +22,9 @@ const Header = () => {
       px={2}
     >
       
-      <img src={chooseUserIcon} alt='user matches icon' />
+      <IconImg src={chooseUserIcon} alt='user matches icon' />
       <img src={logo} alt='logo' />
-      <img src={userMatchesIcon} alt='user matches icon' />
+      <IconImg src={userMatchesIcon} alt='choose user icon' active/>
     </Box>
   )
 }

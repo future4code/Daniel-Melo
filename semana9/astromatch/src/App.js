e2b17d45ddd525bd54b928317fcf93df536a7024
Daniel Melo <danielfarias380@gmail.com>
2020-09-09 14:45:04 -0300

    Initial files and Global Reset with Material-UI

---
+++ semana9/astromatch/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline'
+
+function App() {
+  return (
+    <React.Fragment>
+      <CssBaseline />
+
+      <h1>Hello World</h1>
+    </React.Fragment>  
+  );
+}
+
+export default App;

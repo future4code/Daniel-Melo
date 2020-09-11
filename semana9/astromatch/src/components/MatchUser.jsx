b4d6408f6ee750d2b88d796b03523c49babe2e1c
Daniel Melo <danielfarias380@gmail.com>
2020-09-11 12:07:06 -0300

    cursor pointer and darker bg while hovering on MatchUser component

--- semana9/astromatch/src/components/MatchUser.jsx
+++ semana9/astromatch/src/components/MatchUser.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 
+import { useTheme } from '@material-ui/core/styles'
 import Box from '@material-ui/core/Box';
 import Avatar from '@material-ui/core/Avatar';
 import Typography from '@material-ui/core/Typography'
@@ -10,18 +11,30 @@ const StyledAvatar = styled(Avatar)`
   width: 60px;
 `
 
+const StyledBox = styled(Box)`
+  cursor: pointer;
+  transition: background-color 300ms ease-in;
+
+  :hover {
+    background-color: ${props => props.hoverbg};
+  }
+`
+
 const MatchUser = ({ avatar, name }) => {
+  const theme = useTheme()
+  
   return (
-    <Box
+    <StyledBox
       display="flex"
       alignItems="center"
       minHeight={70}
+      hoverbg={theme.palette.divider}
     >
       <StyledAvatar alt={name} src={avatar} />
       <Box ml={1}>
         <Typography variant='h6'>{name}</Typography>
       </Box>
-    </Box>
+    </StyledBox>
   )
 }
 

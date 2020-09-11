71d793c848c22bab637ba70ac4047e167f626bd0
Daniel Melo <danielfarias380@gmail.com>
2020-09-11 10:24:22 -0300

    implement slide transition when linked or disliked

--- semana9/astromatch/src/components/ChoosePage.jsx
+++ semana9/astromatch/src/components/ChoosePage.jsx
@@ -4,10 +4,12 @@ import styled from 'styled-components'
 import { useTheme } from '@material-ui/core/styles'
 
 import Box from '@material-ui/core/Box'
+import Slide from '@material-ui/core/Slide'
 import Typography from '@material-ui/core/Typography'
 import StyledButton from '../styles/StyledButton'
 import ClearIcon from '@material-ui/icons/Clear';
 import FavoriteIcon from '@material-ui/icons/Favorite';
+import CircularProgress from '@material-ui/core/CircularProgress'
 
 
 const StyledBox = styled(Box)`
@@ -19,19 +21,32 @@ const StyledBox = styled(Box)`
 
 const ChoosePage = () => {
   const theme = useTheme()
+  const [loaded, setLoaded] = useState(false)
   const [profile, setProfile] = useState({})
+  const [choosed, setChoosed] = useState(false)
+  const [liked, setLiked] = useState(false)
 
   const getProfile = () => {
+    setLoaded(false)
     getProfileToChoose()
-      .then(response => setProfile(response.data.profile))
+      .then(response => {
+        setProfile(response.data.profile)
+        setChoosed(false)
+        setLoaded(true)
+      })
       .catch(_ => alert('Erro ao buscar profile'))
   }
 
   const handleChoice = (choice) => {
-    choosePerson(profile.id, choice)
-      .then(response => {
+    setChoosed(true)
+    setLiked(choice)
+    
+  }
+  
+  const getNextProfile = () => {
+    choosePerson(profile.id, liked)
+      .then(_ => {
         getProfile()
-        response.data.isMatch && alert("It's a match!")
       })
       .catch(_ => alert('Erro ao enviar escolha'))
   }
@@ -50,28 +65,55 @@ const ChoosePage = () => {
      height={550}
      width={370}
     >
-      <StyledBox
-        display='flex'
-        flexDirection="column"
-        alignItems='flex-start'
-        justifyContent='flex-end'
-        px={1}
-        pb={2}
-        height={450}
-        width={370}
-        border={1}
-        borderRadius={8}
-        color="common.white"
-        boxShadow={4}
-        bgimage={profile.photo}
-      >
-        <Typography variant='h5'>
-          <strong>{profile.name}</strong>, {profile.age}
-        </Typography>
-        <Typography>
-          {profile.bio}
-        </Typography>
-      </StyledBox>
+      { 
+        loaded 
+        ? 
+          <>
+            <Slide 
+              direction={ liked ? "left" : "right"}
+              in={!choosed}
+              appear={false}
+              onExit={getNextProfile}
+              timeout={500}
+            >
+              <StyledBox
+                display='flex'
+                height={450}
+                width={370}
+                borderRadius={8}
+                boxShadow={4}
+                bgimage={profile.photo}
+                overflow='hidden'
+              >
+                <Box
+                  color='common.white'
+                  bgcolor='text.disabled'
+                  alignSelf="flex-end"
+                  px={1}
+                  pb={1}
+                  width='100%'
+                >
+                  <Typography variant='h5'>
+                    <strong>{profile.name}</strong>, {profile.age}
+                  </Typography>
+                  <Typography>
+                    {profile.bio}
+                  </Typography>
+                </Box>
+              </StyledBox>
+            </Slide>
+          </>
+        : 
+          <Box
+            display='flex'
+            alignItems='center'
+            justifyContent='center'
+            height={450}
+            width={370}
+          >
+            <CircularProgress />
+          </Box>
+      }
 
       <Box
         display='flex'

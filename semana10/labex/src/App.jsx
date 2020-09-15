cfaa4d1ea0214b3a354c3644656c471341909c6d
Daniel Melo <danielfarias380@gmail.com>
2020-09-15 17:14:06 -0300

    Change files extension to jsx

--- semana10/labex/src/App.jsx
+++ semana10/labex/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import ThemeContext from './context/ThemeContext'
-import Routes from './Routes'
+import ThemeContext from './context/ThemeContext';
+import Routes from './Routes';
 import { AuthProvider } from './context/AuthContext';
 
 function App() {

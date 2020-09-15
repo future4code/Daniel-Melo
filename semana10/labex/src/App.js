577a38cf2cb90e85911445fc1c98043cfe49b314
Daniel Melo <danielfarias380@gmail.com>
2020-09-15 16:18:23 -0300

    Wrap components in AuthProvider

--- semana10/labex/src/App.js
+++ semana10/labex/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ThemeContext from './context/ThemeContext'
 import Routes from './Routes'
+import { AuthProvider } from './context/AuthContext';
 
 function App() {
   return (
     <ThemeContext>
-      <Routes />
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
     </ThemeContext>
   );
 }

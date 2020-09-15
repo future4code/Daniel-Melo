44f3b3236ea6384fa5bc408c015b21b9398f1923
Daniel Melo <danielfarias380@gmail.com>
2020-09-15 16:38:15 -0300

    Save logged state in localStorage

--- semana10/labex/src/context/AuthContext.jsx
+++ semana10/labex/src/context/AuthContext.jsx
@@ -1,15 +1,22 @@
 import React, {createContext, useContext, useState} from 'react';
+import { useEffect } from 'react';
 
 const AuthorizationContext = createContext()
 
 export const AuthProvider = ({ children }) => {
   const [isAuthorized, setIsAuthorized] = useState(false)
 
+  useEffect(() => {
+    JSON.parse(localStorage.getItem('logged')) && setIsAuthorized(true)
+  }, [])
+
   const login = () => {
+    localStorage.setItem('logged', JSON.stringify(true))
     setIsAuthorized(true)
   }
 
   const logout = () => {
+    localStorage.removeItem('logged')
     setIsAuthorized(false)
   }
   

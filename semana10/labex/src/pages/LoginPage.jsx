67c04ac88dd02e26a7ec698c028c319802c6bba3
Daniel Melo <danielfarias380@gmail.com>
2020-09-16 14:19:27 -0300

    Implement handleLogin logic

--- semana10/labex/src/pages/LoginPage.jsx
+++ semana10/labex/src/pages/LoginPage.jsx
@@ -2,9 +2,27 @@ import React from 'react';
 import { Heading, Flex, Button } from '@chakra-ui/core';
 import { Link } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
+import api from '../services/api';
 
 const LoginPage = () => {
   const { login } = useAuth();
+
+  const handleLogin = async () => {
+    const body = {
+      email: 'daniel@melo.com',
+      password: '12345',
+    };
+
+    try {
+      const response = await api.post('/login', body);
+      const { token } = response.data;
+      login(token);
+    } catch (error) {
+      if (error.response.status === 401) {
+        console.log(error.response.data.message);
+      }
+    }
+  };
 
   return (
     <Flex justify="space-between">
@@ -13,7 +31,7 @@ const LoginPage = () => {
       </Heading>
 
       <Link to="/trip/list">
-        <Button variantColor="purple" onClick={login}>
+        <Button variantColor="purple" onClick={handleLogin}>
           ENTRAR
         </Button>
       </Link>

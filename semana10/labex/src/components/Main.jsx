0517989a416d10228e9e77e5460824ce114ba996
Daniel Melo <danielfarias380@gmail.com>
2020-09-16 19:59:47 -0300

    Set Main default direction to column

--- semana10/labex/src/components/Main.jsx
+++ semana10/labex/src/components/Main.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Flex } from '@chakra-ui/core';
 
-const Main = ({ column, children }) => (
+const Main = ({ row, children }) => (
   <Flex
     as="main"
-    direction={column ? 'column' : 'row'}
+    direction={row ? 'row' : 'column'}
     flexGrow={1}
     align="center"
-    justify={column ? 'flex-start' : { base: 'center', lg: 'space-between' }}
+    justify={row ? { base: 'center', lg: 'space-between' } : 'flex-start'}
+    pt={row ? 0 : 16}
     px={{ xl: 0, base: 15 }}
     w={{ xl: '1170px', base: '100%' }}
   >

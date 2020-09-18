d19980a34649f8023f4a8627324c51e275160c42
Daniel Melo <danielfarias380@gmail.com>
2020-09-18 15:53:50 -0300

    Import TripList in ListTripPage

--- semana10/labex/src/pages/ListTripPage.jsx
+++ semana10/labex/src/pages/ListTripPage.jsx
@@ -1,43 +1,19 @@
 import React from 'react';
-import {
-  Heading, Flex, Button, Text,
-} from '@chakra-ui/core';
-import { Link } from 'react-router-dom';
-import { useAuth } from '../context/AuthContext';
+import Layout from '../components/Layout';
+import Header from '../components/Header';
+import Main from '../components/Main';
+import PageTitle from '../components/PageTitle';
+import TripList from '../components/TripList';
 
-const ListTripPage = () => {
-  const { logout } = useAuth();
+const ListTripPage = () => (
+  <Layout>
+    <Header isPrivatePage />
 
-  return (
-    <Flex justify="space-between">
-      <Heading>
-        ListTripPage
-      </Heading>
-
-      <Button variantColor="purple" onClick={logout}>
-        SAIR
-      </Button>
-
-      <Link to="/trip/details/1">
-        <Text>Detalhes da Viagem 1</Text>
-      </Link>
-      <Link to="/trip/details/2">
-        <Text>Detalhes da Viagem 2</Text>
-      </Link>
-      <Link to="/trip/details/3">
-        <Text>Detalhes da Viagem 3</Text>
-      </Link>
-      <Link to="/trip/details/4">
-        <Text>Detalhes da Viagem 4</Text>
-      </Link>
-
-      <Link to="/trip/create">
-        <Button variantColor="yellow">
-          Criar Nova Viagem
-        </Button>
-      </Link>
-    </Flex>
-  );
-};
+    <Main>
+      <PageTitle title="Lista de Viagens" />
+      <TripList />
+    </Main>
+  </Layout>
+);
 
 export default ListTripPage;

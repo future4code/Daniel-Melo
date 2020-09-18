4a927f292486419af702476b13038950ce1fff5c
Daniel Melo <danielfarias380@gmail.com>
2020-09-18 19:02:59 -0300

    Implement TripDetailsPage

--- semana10/labex/src/pages/TripDetailsPage.jsx
+++ semana10/labex/src/pages/TripDetailsPage.jsx
@@ -1,31 +1,40 @@
-import React from 'react';
-import { Heading, Flex, Button } from '@chakra-ui/core';
-import { Link, useParams } from 'react-router-dom';
-import { useAuth } from '../context/AuthContext';
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import api from '../services/api';
+import Layout from '../components/Layout';
+import Header from '../components/Header';
+import Main from '../components/Main';
+import PageTitle from '../components/PageTitle';
+import TripDetails from '../components/TripDetails';
+import CandidateList from '../components/CandidateList';
+import axiosErrorHandler from '../utils/axiosErrorHandler';
 
 const TripDetailsPage = () => {
   const { id } = useParams();
-  const { logout } = useAuth();
+  const [tripDetails, setTripDetails] = useState({ trip: {} });
+
+  useEffect(() => {
+    api.get(`/trip/${id}`)
+      .then(({ data }) => {
+        setTripDetails(data);
+      })
+      .catch(axiosErrorHandler);
+  }, [id]);
 
   return (
-    <Flex justify="space-between">
-      <Heading>
-        TripDetailsPage
-        {' '}
-        {id}
-      </Heading>
+    <Layout>
+      <Header isPrivatePage withBackButton />
 
-      <Link to="/trip/list">
-        <Button variantColor="yellow">
-          VOLTAR
-        </Button>
-      </Link>
-
-      <Button variantColor="purple" onClick={logout}>
-        SAIR
-      </Button>
-    </Flex>
+      <Main>
+        <PageTitle title={tripDetails.trip.name} justified />
+        <TripDetails trip={tripDetails.trip} />
+        <CandidateList
+          approveds={tripDetails.trip.approved}
+          candidates={tripDetails.trip.candidates}
+          tripId={tripDetails.trip.id}
+        />
+      </Main>
+    </Layout>
   );
 };
-
 export default TripDetailsPage;

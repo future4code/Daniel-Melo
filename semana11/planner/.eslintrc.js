357c5af442611e326cb354c39f6b73da996945f7
Daniel Melo <danielfarias380@gmail.com>
2020-09-23 16:54:23 -0300

    Initial files

---
+++ semana11/planner/.eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:testing-library/recommended',
+    'plugin:jest-dom/recommended',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'jest-dom',
+    'testing-library',
+  ],
+  rules: {
+  },
+};

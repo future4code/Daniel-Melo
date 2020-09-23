ec1f27425a65cffed87dd2000728fdcf410f657f
Daniel Melo <danielfarias380@gmail.com>
2020-09-23 10:45:00 -0300

    Add beforeEach to render the App component and create a post

--- semana11/teste-react/src/App.test.js
+++ semana11/teste-react/src/App.test.js
@@ -1,142 +1,94 @@
 import React from "react";
-import { render, fireEvent, queryByPlaceholderText } from "@testing-library/react";
+import { render, fireEvent, screen } from "@testing-library/react";
 import App from "./App";
 
 
 describe('Feed Page Test' , () => {
-  test("user should be able to create new post", () => {
-    // Arrange
-    const { queryByText, getByText, getByPlaceholderText } = render(<App />) 
-  
+  beforeEach(() => {
+    render(<App />)
+
     // 1. usuário digita o post
-    fireEvent.change(getByPlaceholderText(/Novo post/i), {
+    fireEvent.change(screen.getByPlaceholderText(/Novo post/i), {
       target: { value: 'Um post legal'}
     })
     // 2. usuário clica em Adicionar
-    fireEvent.click(getByText(/Adicionar/i))
+    fireEvent.click(screen.getByText(/Adicionar/i))
+  })
 
+  test("user should be able to create new post", () => {
     // Assert
-    expect(queryByText(/Um post legal/)).toBeTruthy()
-    expect(queryByText(/Curtir/)).toBeTruthy()
-    expect(queryByText(/Apagar/)).toBeTruthy()
+    expect(screen.queryByText(/Um post legal/)).toBeTruthy()
+    expect(screen.queryByText(/Curtir/)).toBeTruthy()
+    expect(screen.queryByText(/Apagar/)).toBeTruthy()
   })
   
   test("user should be able to like a post", () => {
-    // Arrange
-    const { queryByText, getByText, getByPlaceholderText } = render(<App />) 
-  
-    // 1. usuário digita o post
-    fireEvent.change(getByPlaceholderText(/Novo post/i), {
-      target: { value: 'Um post legal'}
-    })
-    // 2. usuário clica em Adicionar
-    fireEvent.click(getByText(/Adicionar/i))
-    // 3. usuário clica em Curtir
-    fireEvent.click(getByText(/Curtir/))
+    // Act
+    fireEvent.click(screen.getByText(/Curtir/))
   
     // Assert
-    expect(queryByText(/Descurtir/)).toBeTruthy()
+    expect(screen.queryByText(/Descurtir/)).toBeTruthy()
   })
 
   test("user should be able to delete a post", () => {
-    // Arrange
-    const { queryByText, getByText, getByPlaceholderText } = render(<App />) 
-  
-    // 1. usuário digita o post
-    fireEvent.change(getByPlaceholderText(/Novo post/i), {
-      target: { value: 'Um post legal'}
-    })
-    // 2. usuário clica em Adicionar
-    fireEvent.click(getByText(/Adicionar/i))
-    // 3. usuário clica em Apagar
-    fireEvent.click(getByText(/Apagar/))
+    // Act
+    fireEvent.click(screen.getByText(/Apagar/))
   
     // Assert
-    expect(queryByText(/Um post legal/)).toBe(null)
+    expect(screen.queryByText(/Um post legal/)).toBeNull()
   })
 
   test("input should be cleared when post has been created", () => {
-    // Arrange
-    const {getByText, getByPlaceholderText, queryByPlaceholderText } = render(<App />) 
-  
-    // 1. usuário digita o post
-    fireEvent.change(getByPlaceholderText(/Novo post/i), {
-      target: { value: 'Um post legal'}
-    })
-    // 2. usuário clica em Adicionar
-    fireEvent.click(getByText(/Adicionar/i))
-      
     // Assert 
-    expect(queryByPlaceholderText(/Novo post/i).value).toEqual('')
+    expect(screen.queryByPlaceholderText(/Novo post/i)).toHaveValue('')
   })
 
   test("when the post list is empty, should appear 'Nenhum post' message", () => {
-    // Arrange
-    const { queryByText } = render(<App />)
-
+    // Act - apaga post
+    fireEvent.click(screen.getByText(/Apagar/))
+    
     // Assert
-    expect(queryByText(/Nenhum post/i)).toBeTruthy()
+    expect(screen.queryByText(/Nenhum post/i)).toBeTruthy()
   })
   
   test("when the post list is not empty, should not appear 'Nenhum post' message", () => {
-    // Arrange
-    const { queryByText, getByPlaceholderText, getByText } = render(<App />)
-
-    // 1. usuário digita o post
-    fireEvent.change(getByPlaceholderText(/Novo post/i), {
-      target: { value: 'Um post legal'}
-      })
-    // 2. usuário clica em Adicionar
-    fireEvent.click(getByText(/Adicionar/i))
-
     // Assert
-    expect(queryByText(/Nenhum post/i)).toEqual(null)
+    expect(screen.queryByText(/Nenhum post/i)).toBeNull()
   })
   
   test("Post quantity should be visible when there is any post", () => {
-    // Arrange
-    const { queryByText, getByPlaceholderText, getByText } = render(<App />)
-
-    // 1. usuário digita o post
-    fireEvent.change(getByPlaceholderText(/Novo post/i), {
-      target: { value: 'Um post legal'}
-      })
-    // 2. usuário clica em Adicionar
-    fireEvent.click(getByText(/Adicionar/i))
-    // 3. usuário digita o post
-    fireEvent.change(getByPlaceholderText(/Novo post/i), {
+    // Act
+    // 1. usuário digita o post 2
+    fireEvent.change(screen.getByPlaceholderText(/Novo post/i), {
       target: { value: 'Um post legal 2'}
       })
-    // 4. usuário clica em Adicionar
-    fireEvent.click(getByText(/Adicionar/i))
-    // 5. usuário digita o post
-    fireEvent.change(getByPlaceholderText(/Novo post/i), {
+    // 2. usuário clica em Adicionar
+    fireEvent.click(screen.getByText(/Adicionar/i))
+    // 3. usuário digita o post 3
+    fireEvent.change(screen.getByPlaceholderText(/Novo post/i), {
       target: { value: 'Um post legal 3'}
       })
-    // 6. usuário clica em Adicionar
-    fireEvent.click(getByText(/Adicionar/i))
+    // 4. usuário clica em Adicionar
+    fireEvent.click(screen.getByText(/Adicionar/i))
 
     // Assert
-    expect(queryByText(/Quantidade de posts: 3/i)).toBeTruthy()
+    expect(screen.queryByText(/Quantidade de posts: 3/i)).toBeTruthy()
   })
   
   test("Post quantity should not be visible when there is no post", () => {
-    // Arrange
-    const { queryByText } = render(<App />)
+    // Act - apaga post
+    fireEvent.click(screen.getByText(/Apagar/))
 
     // Assert
-    expect(queryByText(/Quantidade de posts/i)).toEqual(null)
+    expect(screen.queryByText(/Quantidade de posts/i)).toBeNull()
   })
 
   test("User can not create an empty post, a message should be visible 'Não é permitido criar post vazio'", () => {
-    // Arrange
-    const { queryByText, getByText } = render(<App />) 
-  
     // Act
     // 1. usuário clica em Adicionar sem inserir nada no input
-    fireEvent.click(getByText(/Adicionar/i))
+    fireEvent.click(screen.getByText(/Adicionar/i))
 
     // Assert
-    expect(queryByText(/Não é permitido criar post vazio/i)).toBeTruthy()
+    expect(screen.queryByText(/Não é permitido criar post vazio/i)).toBeTruthy()
   })
 })

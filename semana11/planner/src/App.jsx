1eb83bab0a530be1b9c9bda825250a7d47676c8e
Daniel Melo <danielfarias380@gmail.com>
2020-09-25 16:48:11 -0300

    feat: App implementation

--- semana11/planner/src/App.jsx
+++ semana11/planner/src/App.jsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import CreateTaskForm from './components/CreateTaskForm';
+import WeekDayCard from './components/WeekDayCard';
+import api from './services/api';
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const getTasks = async () => {
+    const response = await api.get('');
+    setTasks(response.data);
+  };
+
+  useEffect(() => {
+    getTasks();
+  }, []);
+
+  const weekDays = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
+
   return (
     <div className="App">
-      <CreateTaskForm />
+      <CreateTaskForm update={getTasks} />
+      {weekDays.map((weekDay) => (
+        <WeekDayCard key={weekDay} weekDay={weekDay} tasks={tasks} update={getTasks} />
+      ))}
     </div>
   );
 }

6025d482b0218d0210c3ea7d2f9cc387b11d0ade
Daniel Melo <danielfarias380@gmail.com>
2020-09-29 19:15:19 -0300

    Implement authorization procedure

--- semana12/labeddit/src/components/LoginForm.jsx
+++ semana12/labeddit/src/components/LoginForm.jsx
@@ -4,14 +4,25 @@ import {
 } from '@chakra-ui/core';
 import logo from '../assets/main_logo.svg';
 import { useAuth } from '../contexts/AuthProvider';
+import useForm from '../hooks/useForm';
+import api from '../services/api';
 
 const LoginForm = () => {
   const { login } = useAuth();
+  const [form, setForm, resetForm] = useForm({
+    email: '',
+    password: '',
+  });
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
-    login();
+    try {
+      const { data } = await api.post('/login', form);
+      login(data);
+    } catch (error) {
+      resetForm();
+    }
   };
 
   return (
@@ -24,8 +35,8 @@ const LoginForm = () => {
       onSubmit={handleSubmit}
     >
       <Image src={logo} alt="logo" />
-      <Input id="login-email" mt={4} type="email" placeholder="E-mail" isRequired />
-      <Input id="login-password" mt={4} type="password" placeholder="Senha" isRequired />
+      <Input id="login-email" name="email" mt={4} type="email" placeholder="E-mail" isRequired value={form.email} onChange={setForm} />
+      <Input id="login-password" name="password" mt={4} type="password" placeholder="Senha" isRequired value={form.password} onChange={setForm} />
       <Button type="submit" mt={8} colorScheme="blue">Entrar</Button>
     </FormControl>
   );
--- semana12/labeddit/src/contexts/AuthProvider.jsx
+++ semana12/labeddit/src/contexts/AuthProvider.jsx
@@ -1,20 +1,42 @@
-import React, { useState, useContext, createContext } from 'react';
+import React, {
+  useState, useEffect, useContext, createContext,
+} from 'react';
+import api from '../services/api';
 
 const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
   const [loggedIn, setLoggedIn] = useState(false);
+  const [user, setUser] = useState({});
 
-  const login = () => {
+  const login = ({ token, user: userInfo }) => {
+    localStorage.setItem('token', token);
+    localStorage.setItem('user', JSON.stringify(userInfo));
+    api.defaults.headers.common.Authorization = token;
     setLoggedIn(true);
   };
 
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    const userInfo = JSON.parse(localStorage.getItem('user'));
+    if (token && userInfo) {
+      login({ token, user: userInfo });
+    }
+  }, []);
+
   const logout = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+    delete api.defaults.headers.common.Authorization;
     setLoggedIn(false);
+    setUser({});
   };
 
   return (
-    <AuthContext.Provider value={{ loggedIn, login, logout }}>
+    <AuthContext.Provider value={{
+      loggedIn, user, login, logout,
+    }}
+    >
       {children}
     </AuthContext.Provider>
   );

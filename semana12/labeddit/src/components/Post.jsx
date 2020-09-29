c80f43ca2d8e7fafea3be12d8a12f40c7eab0ab0
Daniel Melo <danielfarias380@gmail.com>
2020-09-29 20:16:52 -0300

    Add date of creaton to post card

--- semana12/labeddit/src/components/Post.jsx
+++ semana12/labeddit/src/components/Post.jsx
@@ -6,37 +6,43 @@ import { ChatIcon } from '@chakra-ui/icons';
 import { Link } from 'react-router-dom';
 import VoteCounter from './VoteCounter';
 
-const Post = ({ post }) => (
-  <VStack
-    as="article"
-    align="flex-start"
-    p={4}
-    mt={4}
-    boxShadow="0px 0px 5px 1px rgba(0,0,0,0.25)"
-    borderRadius={8}
-  >
-    <Heading size="lg">
-      <Link to={`/post/${post.id}`}>
-        {post.title}
-      </Link>
-    </Heading>
-    <Text color="gray.500">{post.username}</Text>
-    <Text>{post.text}</Text>
+const Post = ({ post }) => {
+  const createdAt = new Date(post.createdAt);
 
-    <HStack alignSelf="stretch" justify="space-between">
-      <VoteCounter
-        voteDirection={post.userVoteDirection}
-        votesCount={post.votesCount}
-      />
+  return (
+    <VStack
+      as="article"
+      align="flex-start"
+      p={4}
+      mt={4}
+      boxShadow="0px 0px 5px 1px rgba(0,0,0,0.25)"
+      borderRadius={8}
+    >
+      <Heading size="md">
+        <Link to={`/post/${post.id}`}>
+          {post.title}
+        </Link>
+      </Heading>
+      <Text fontSize="sm" color="gray.500">
+        {`${post.username} - ${createdAt.toLocaleString('pt-BR')}`}
+      </Text>
+      <Text>{post.text}</Text>
 
-      <HStack>
-        <ChatIcon />
-        <Text>
-          {post.commentsCount}
-        </Text>
+      <HStack alignSelf="stretch" justify="space-between">
+        <VoteCounter
+          voteDirection={post.userVoteDirection}
+          votesCount={post.votesCount}
+        />
+
+        <HStack>
+          <ChatIcon />
+          <Text>
+            {post.commentsCount}
+          </Text>
+        </HStack>
       </HStack>
-    </HStack>
-  </VStack>
-);
+    </VStack>
+  );
+};
 
 export default Post;

105880253227fae1d8928e2022f02525bafad7b1
Daniel Melo <danielfarias380@gmail.com>
2020-09-29 13:54:05 -0300

    Add login button on LoginPage

--- semana12/labeddit/src/pages/LoginPage.jsx
+++ semana12/labeddit/src/pages/LoginPage.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Button } from '@chakra-ui/core';
+import { useAuth } from '../contexts/AuthProvider';
 
-const LoginPage = () => (
-  <div>
-    Login Page
-  </div>
-);
+const LoginPage = () => {
+  const { login } = useAuth();
 
+  return (
+    <div>
+      Login Page
+      <Button type="button" onClick={login} colorScheme="blue">Login</Button>
+    </div>
+  );
+};
 export default LoginPage;
